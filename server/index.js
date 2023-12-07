715dd0c8b9699f712a0b18b8976ea6252c6ecef9
Ryan <rbass6@uncc.edu>
2023-12-07 18:10:55 -0500

    Add react app

--- server/index.js
+++ server/index.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const cors = require('cors');
+
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+app.use(cors());
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
 
-app.listen(port, () => console.log(`Example app listening on port ${port}`));
+app.listen(port, () => {
+  console.log('Server started on port', port);
+});
