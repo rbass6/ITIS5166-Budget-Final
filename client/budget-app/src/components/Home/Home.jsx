98a8cd458b95b101027f52fe085e0b4371bd0d8d
Ryan <97636691+rbass6@users.noreply.github.com>
2023-12-11 23:08:56 -0500

    Add intro page

--- client/budget-app/src/components/Home/Home.jsx
+++ client/budget-app/src/components/Home/Home.jsx
@@ -3,16 +3,31 @@ import Navibar from '../Navibar/Navibar';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import { Outlet } from "react-router-dom"
+import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 import axios from 'axios';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 import './Home.css';
 
 export default function Home({loggedIn, setLoggedIn, showLogoutWarning, setShowLogoutWarning, setWarningShown, reset}) {
 
+  const navigate = useNavigate();
   const url = process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : '';
+  const [showIntro, setShowIntro] = useState(true);
 
   function handleClose() {
     setShowLogoutWarning(false);
     setWarningShown(true);
+  }
+
+  function handleLogin() {
+    setShowIntro(false);
+    navigate('/login');
+  }
+
+  function handleRegister() {
+    setShowIntro(false);
+    navigate('/register');
   }
 
   function handleStayLoggedIn() {
@@ -41,6 +56,19 @@ export default function Home({loggedIn, setLoggedIn, showLogoutWarning, setShowL
     <div className="home">
       <Container fluid className="app-container">
         <Navibar loggedIn={loggedIn} setLoggedIn={setLoggedIn}/>
+        {
+          !loggedIn && showIntro && (
+            <div className="home-default">
+              <i class="bi bi-wallet2"></i>
+              <h3>Budget BFF</h3>
+              <h4><i>"Your best friend for budgeting!"</i></h4>
+              <div className="home-button-container">
+                <Button variant="primary" className="home-login-button" onClick={handleLogin}>Login</Button>
+                <Button variant="primary" className="home-register-button" onClick={handleRegister}>Register</Button>
+              </div>
+            </div>
+          )
+        }
         <Outlet />
       </Container>
       <Modal show={showLogoutWarning} onHide={handleClose} centered>
